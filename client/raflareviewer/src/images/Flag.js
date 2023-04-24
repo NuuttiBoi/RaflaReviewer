2273b41d7919baba354b996ab4bef666a67b86be
matleek <104016919+matleek@users.noreply.github.com>
2023-04-24 13:38:35 +0300

    kommentteihin kuvakkeet

---
+++ client/raflareviewer/src/images/Flag.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Icon() {
+  return (
+    <div className="commentIcon">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="-1 0 24 24"
+            >
+            <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeWidth="2"
+                d="M6 13V5.694c0-.317 0-.475.104-.55.104-.075.254-.025.554.075l9.919 3.307c.588.196.882.294.882.474s-.294.278-.882.474L6 13zm0 0v6"
+            ></path>
+        </svg>
+    </div>
+  );
+}
+
+export default Icon;
