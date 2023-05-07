d9605d1c31b7ab7e86e8c3024c23e6eabf1ee018
Onsbohh <104371139+Onsbohh@users.noreply.github.com>
2023-05-07 19:01:48 +0300

    JSDoc lisää

--- client/raflareviewer/src/services/restaurants.js
+++ client/raflareviewer/src/services/restaurants.js
@@ -3,22 +3,42 @@
 import axios from 'axios'
 const baseUrl = 'http://localhost:3001/api/v1/restaurants'
 
+/**
+ * Hakee kaikki ravontolat.
+ * @returns {Promise<any>}
+ */
 const getAll = async () => {
     const response = await axios.get(baseUrl)
     return await response.data
 }
 
+/**
+ * Hakee yksittäisen ravintolan ID:llä.
+ * @param id - Ravintolan ID, joka haetaan.
+ * @returns {Promise<any>}
+ */
 const getRestaurant = async (id) => {
     const response = await axios.get(`${baseUrl}/${id}`)
     return await response.data
 }
 
+/**
+ * Luo uuden ravintolan.
+ * @param newObject - Objekti, jonka sisällä ravintolan tiedot, joka luodaan.
+ * @returns {Promise<any>}
+ */
 const create = async (newObject) => {
     const response = await axios.post(baseUrl, newObject)
     console.log('axios success')
     return await response.data
 }
 
+/**
+ * Päivittää ravintolan sen ID:llä.
+ * @param id - Ravintolan ID, joka päivitetään.
+ * @param newObject - Tiedot, jonka avulla päivitetään.
+ * @returns {Promise<any>}
+ */
 const updateRestaurant = async (id, newObject) => {
     console.log('axios, id: ', id)
     console.log('axios, newObj: ', newObject)
@@ -27,6 +47,11 @@ const updateRestaurant = async (id, newObject) => {
     return await response.data
 }
 
+/**
+ * Ravintolan poisto ID:llä.
+ * @param id - Ravintolan ID, joka poistetaan.
+ * @returns {Promise<axios.AxiosResponse<any>>}
+ */
 const deleteRestaurant = async (id) => {
     const response = await axios.delete(`${baseUrl}/${id}`)
     return response
